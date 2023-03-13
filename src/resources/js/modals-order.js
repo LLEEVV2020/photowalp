cb54953d789f3a4fcfd81953192de614a925f3a2
Mizam <magaramov.top@gmail.com>
2023-03-13 16:31:47 +0300

    Модальные окна в order

---
+++ src/resources/js/modals-order.js
@@ -0,0 +1,26 @@
+modal__texture__btn.onclick = function(){
+	modal__unique.classList.remove('display__none');
+	modal__texture.classList.remove('display__none');
+}
+modal__texture__btn__close.onclick = function(){
+	modal__unique.classList.add('display__none');
+	modal__texture.classList.add('display__none');
+}
+
+modal__instructions__btn.onclick = function(){
+	modal__unique.classList.remove('display__none');
+	modal__instructions.classList.remove('display__none');
+}
+modal__instructions__btn__close.onclick = function(){
+	modal__unique.classList.add('display__none');
+	modal__instructions.classList.add('display__none');
+}
+
+modal__basket__btn.onclick = function(){
+	modal__unique.classList.remove('display__none');
+	modal__bascet.classList.remove('display__none');
+}
+modal__basket__btn__close.onclick = function(){
+	modal__unique.classList.add('display__none');
+	modal__bascet.classList.add('display__none');
+}
