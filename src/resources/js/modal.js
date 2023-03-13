db1807b038a3a01160674c55ce20c584a5f4aefc
Mizam <magaramov.top@gmail.com>
2023-03-13 13:11:38 +0300

    Модалка хедер

--- src/resources/js/modal.js
+++ src/resources/js/modal.js
@@ -1,3 +1,13 @@
+modal_btn_open_head.onclick = function(){
+	modal_head.classList.add('show');
+}
+modal_btn_close_head.onclick = function(){
+	modal_head.classList.remove('show')
+}
+overlay_head.onclick = function(){
+	modal_head.classList.remove('show')
+}
+
 modal_btn_open.onclick = function(){
 	modal.classList.add('show');
 }
