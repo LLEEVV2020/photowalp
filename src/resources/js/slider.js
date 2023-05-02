00272627baaa50b53c0a2497c6644b3989424da9
VaysLu <vays3btro@gmail.com>
2023-05-03 01:31:33 +0300

    commit

--- src/resources/js/slider.js
+++ src/resources/js/slider.js
@@ -12,30 +12,14 @@ $(document).ready(function () {
       variableWidth: false,
       infinite: false,
       draggable: false,
-      // responsive: [
-      //    {
-      //       breakpoint: 1024,
-      //       settings: {
-      //          slidesToShow: 3,
-      //          slidesToScroll: 1,
-      //          infinite: true,
-      //          dots: false
-      //       }
-      //    },
-      //    {
-      //       breakpoint: 600,
-      //       settings: {
-      //          slidesToShow: 2,
-      //          slidesToScroll: 2
-      //       }
-      //    },
-      //    {
-      //       breakpoint: 480,
-      //       settings: {
-      //          slidesToShow: 1,
-      //          slidesToScroll: 1
-      //       }
-      //    }
-      // ]
+      swipe: false,
+      responsive: [
+         {
+            breakpoint: 822,
+            settings: {
+               swipe: true,
+            }
+         }
+      ]
    });
 });
