1f5d52defc362d905f411cfdd634b52a96e0357d
VaysLu <vays3btro@gmail.com>
2023-05-02 23:03:08 +0300

    Commit Slider in Order Page autor VaysLu

---
+++ src/resources/js/slider.js
@@ -0,0 +1,40 @@
+
+// ============ Sliders ==============
+$(document).ready(function () {
+   $('.sliders__order').slick({
+      arrows: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 500,
+      easing: 'ease',
+      centerMode: true,
+      variableWidth: false,
+      infinite: false,
+      draggable: false,
+      // responsive: [
+      //    {
+      //       breakpoint: 1024,
+      //       settings: {
+      //          slidesToShow: 3,
+      //          slidesToScroll: 1,
+      //          infinite: true,
+      //          dots: false
+      //       }
+      //    },
+      //    {
+      //       breakpoint: 600,
+      //       settings: {
+      //          slidesToShow: 2,
+      //          slidesToScroll: 2
+      //       }
+      //    },
+      //    {
+      //       breakpoint: 480,
+      //       settings: {
+      //          slidesToShow: 1,
+      //          slidesToScroll: 1
+      //       }
+      //    }
+      // ]
+   });
+});
