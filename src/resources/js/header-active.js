e5996ab55323e6e27a0e0ae1a5605f22adce1bbe
VaysLu <vays3btro@gmail.com>
2023-05-18 23:26:29 +0300

    commit

--- src/resources/js/header-active.js
+++ src/resources/js/header-active.js
@@ -1,3 +1,6 @@
 document.querySelectorAll('.header__menu > ul > li > a').forEach((n, i, a) => {
     n.addEventListener('click', () => a.forEach(m => m.classList.toggle('is-active', m === n)));
 });
+function viewDiv() {
+    document.getElementById("modal_btn_open_head").classList.remove('is-active');
+};
