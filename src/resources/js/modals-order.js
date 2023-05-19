2a143dbda902e2676f62172775606fcd49dd12cd
VaysLu <vays3btro@gmail.com>
2023-05-19 14:44:18 +0300

    commit

--- src/resources/js/modals-order.js
+++ src/resources/js/modals-order.js
@@ -1,26 +1,30 @@
-modal__texture__btn.onclick = function(){
+// modal__texture__btn.onclick = function(){
+// 	modal__unique.classList.remove('display__none');
+// 	modal__texture.classList.remove('display__none');
+// }
+document.getElementsByClassName("texture_element").onclick = function () {
 	modal__unique.classList.remove('display__none');
 	modal__texture.classList.remove('display__none');
 }
-modal__texture__btn__close.onclick = function(){
+modal__texture__btn__close.onclick = function () {
 	modal__unique.classList.add('display__none');
 	modal__texture.classList.add('display__none');
 }
 
-modal__instructions__btn.onclick = function(){
+modal__instructions__btn.onclick = function () {
 	modal__unique.classList.remove('display__none');
 	modal__instructions.classList.remove('display__none');
 }
-modal__instructions__btn__close.onclick = function(){
+modal__instructions__btn__close.onclick = function () {
 	modal__unique.classList.add('display__none');
 	modal__instructions.classList.add('display__none');
 }
 
-modal__basket__btn.onclick = function(){
+modal__basket__btn.onclick = function () {
 	modal__unique.classList.remove('display__none');
 	modal__bascet.classList.remove('display__none');
 }
-modal__basket__btn__close.onclick = function(){
+modal__basket__btn__close.onclick = function () {
 	modal__unique.classList.add('display__none');
 	modal__bascet.classList.add('display__none');
 }
